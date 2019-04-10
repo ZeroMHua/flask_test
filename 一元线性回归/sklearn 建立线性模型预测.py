eb09cf2055b974f944b27a87baad4c4e8f490782
tanmenghua <tanmenghua1@aidcloud.cn>
2019-04-10 17:06:18 +0800

    sklearn建立线性模型预测,R检验模型的拟合度

--- 一元线性回归/sklearn 建立线性模型预测.py
+++ 一元线性回归/sklearn 建立线性模型预测.py
@@ -19,3 +19,19 @@ a = model.predict([[5]])
 print(a)
 # a[0][0]
 print("预测一张12英寸匹萨价格：{:.2f}".format(model.predict([[5]])[0][0]))
+
+print('残差平方和:{:.2f}'.format(np.mean((model.predict(X) - y) ** 2)))
+
+"""
+有些度量方法可以用来评估预测效果，我们用R方（r-squared）评估匹萨价格预测的效果。R方也叫 
+确定系数（coefficient of determination），表示模型对现实数据拟合的程度。计算R方的方法有几 
+种。一元线性回归中R方等于皮尔逊积矩相关系数（Pearson product moment correlation coefficient 
+或Pearson’s r）的平方
+"""
+# 测试集
+X_test = [[8], [9], [11], [16], [12]]
+y_test = [[11], [8.5], [15], [18], [11]]
+model = linear_model.LinearRegression()
+model.fit(X, y)
+R = model.score(X_test, y_test)
+print(R)
