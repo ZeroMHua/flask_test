01f9d691f466572bf865622905d0bc2bfe9425d0
tanmenghua <tanmenghua1@aidcloud.cn>
2019-04-22 14:10:50 +0800

    接收返回值有中文的情况，key为中文的情况

---
+++ 服务端和请求示例/请求示例.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:hua
+import requests
+import json
+
+url = "http://127.0.0.1:5000/sklearn"
+# url = "http://192.168.11.220:9900/ochiias"
+
+data = {
+        # "list1":[x for x in range(1000000)]
+        # "list1":[93,62,51,93,75,82,93,62,65,51,86,89,100]
+    "你":[[0., 0.], [1., 1.], [2., 2.], [3., 3.]],
+    # "X":[],
+    "Y":[0., 1., 2., 4.],
+    "pdic":[1, 0.]
+}
+data=json.dumps(data,ensure_ascii=True)
+headers = {'Content-Type': 'application/json'}
+response = requests.post(url,data,headers=headers)
+
+data1 = response.content.decode(encoding="unicode-escape")
+
+# data1 = response.content.decode(encoding="utf-8")
+# data = data.encode("unicode-escape").decode("utf-8")
+# data = data.encode("unicode-escape").decode("gb18030")
+# 获取key为中文的返回值
+data2 = json.loads(data1)["uu"]['你好']
+print(data2)
+
