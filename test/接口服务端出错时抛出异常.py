c8b3a1a9b7c261435feab69c18e56b1218f607a0
tanmenghua <tanmenghua1@aidcloud.cn>
2019-04-22 15:13:58 +0800

    不知参数名的情况下获取参数和期对应的值

--- test/接口服务端出错时抛出异常.py
+++ test/接口服务端出错时抛出异常.py
@@ -13,10 +13,17 @@ def index():
     from sklearn import linear_model
     from sklearn.linear_model import BayesianRidge
     data = request.json
-
+    list1 = []
+    for key, value in data.items():
+        list1.append(key)
+    # da1 = [data[key] for key in list1]
+    # print(da1)
+    # 如果传的参数顺序不变
     # X = [[0., 0.], [1., 1.], [2., 2.], [3., 3.]]
-    X = data["你"]
-    Y = data["Y"]
+    # X = data["你"]
+    X = data[list1[0]]
+    # Y = data["Y"]
+    Y = data[list1[1]]
     # Y = [0., 1., 2., 3.]
     try:
         reg = linear_model.BayesianRidge()
@@ -32,8 +39,7 @@ def index():
 
         return jsonify(erro=e)
 
-    return jsonify(xx=xx, Y=Y, uu={'你好': [1, 0.2, 3.5],
-                                   '好好': 0.35,'95%区间上限':-38.038},yy={'年龄':{'N值':12}})
+    return jsonify(xx=xx, Y=Y, uu={list1[1]: 0.35},yy={list1[0]:{'N值':12}})
 
 
 if __name__ == '__main__':
