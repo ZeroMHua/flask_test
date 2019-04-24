14c44563881d94ff5913e8f9f3a93294f6ea269f
tanmenghua <tanmenghua1@aidcloud.cn>
2019-04-24 09:21:02 +0800

    接收多个参数

--- 服务端和请求示例/仿照R返回对应的信息/t检验客户端.py
+++ 服务端和请求示例/仿照R返回对应的信息/t检验客户端.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # Author:hua
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding:utf-8 -*-
 # Author:hua
 import requests
@@ -12,19 +12,19 @@ url = "http://127.0.0.1:5000/sklearn"
 # url = "http://192.168.11.220:5000/sklearn"
 
 data = {
-    "data":[
+    "data": [
         {"第1个参数": [x for x in range(10)],
-         "第2个参数": [y for y in range(10,21)],
-         "第三个参数": [z for z in range(20, 31)]},
-
-
-
-        {"置信水平":0.95}
-        ]
+         "第2个参数": [y for y in range(110, 121)],
+         "第三个参数": [z for z in range(20, 31)],
+         "第4个参数": [z for z in range(120, 131)],
+         "第5个参数": [z for z in range(210, 311)]
+         },
+        {"置信水平": 0.95}
+    ]
 }
-data=json.dumps(data,ensure_ascii=True)
+data = json.dumps(data, ensure_ascii=True)
 headers = {'Content-Type': 'application/json'}
-response = requests.post(url,data,headers=headers)
+response = requests.post(url, data, headers=headers)
 
 data1 = response.content.decode(encoding="unicode-escape")
 
@@ -33,4 +33,3 @@ data2 = json.loads(data1)
 # 提取第一个表的信息
 # data3 = data2["yy"]["data"][0]['tables'][0]["colNames"]
 print(data2)
-
