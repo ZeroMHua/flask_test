997c54db7d46a2e0db66db9107e969c6aab46de7
tanmenghua <tanmenghua1@aidcloud.cn>
2019-04-28 18:50:16 +0800

    b 字节流保存为图片

--- 字节流保存为图片/b'流保存为图片.py
+++ 字节流保存为图片/b'流保存为图片.py
@@ -2,13 +2,15 @@
 # -*- coding:utf-8 -*-
 # Author:hua
 import requests
-
-# url = "http://www.hypathology.com:8092/viewer2/StreamHandler.ashx?SlideID=-1&kfbpath=D:\Slides\hy\KB1902156\KB1902156-1_2019-04-06%2010_36_33.kfb&Zoom=20&FileNum=15&level=12&PositionX=%s&PositionY=%s&TileSize=256&gamma=1.2&contrast=1&light=0&rgbR=0&rgbG=0&rgbB=0" % (35,38)
-url = "http://www.hypathology.com:8092/viewer2/StreamHandler.ashx?SlideID=-1&kfbpath=D:\Slides\hy\KB1902156\KB1902156-1_2019-04-06%2010_36_33.kfb&Zoom=20&FileNum=15&level=12&PositionX=3&PositionY=4&TileSize=256&gamma=1.2&contrast=1&light=0&rgbR=0&rgbG=0&rgbB=0"
+for X in range(61):
+    for Y in range(77):
+        url = "http://www.hypathology.com:8092/viewer2/StreamHandler.ashx?SlideID=-1&kfbpath=D:\Slides\hy\KB1902156\KB1902156-1_2019-04-06%2010_36_33.kfb&Zoom=20&FileNum=15&level=15&PositionX={0}&PositionY={1}&TileSize=256&gamma=1.2&contrast=1&light=0&rgbR=0&rgbG=0&rgbB=0".format(X,Y)
+# url = "http://www.hypathology.com:8092/viewer2/StreamHandler.ashx?SlideID=-1&kfbpath=D:\Slides\hy\KB1902156\KB1902156-1_2019-04-06%2010_36_33.kfb&Zoom=20&FileNum=15&level=12&PositionX=3&PositionY=4&TileSize=256&gamma=1.2&contrast=1&light=0&rgbR=0&rgbG=0&rgbB=0"
 url1 ="http://www.hypathology.com:8092/viewer2/StreamHandler.ashx?SlideID=-1&kfbpath=D:\Slides\hy\KB1902156\KB1902156-2_2019-04-06%2010_37_08.kfb&Zoom=20&FileNum=15&level=15&PositionX=35&PositionY=38&TileSize=256&gamma=1.2&contrast=1&light=0&rgbR=0&rgbG=0&rgbB=0"
-response = requests.get(url1)
+response = requests.get(url)
 data = response.content
-print(data)
-file = open('test.jpg', 'wb')
+# print(data)
+# name = str(X)+str(Y)
+file = open('TEST1.jpg', 'wb')
 file.write(data)
 file.close()
---
+++ 字节流保存为图片/test/b'流保存为图片.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:hua
+import requests
+
+for X in range(61):
+    for Y in range(77):
+        url = "http://www.hypathology.com:8092/viewer2/StreamHandler.ashx?SlideID=-1&kfbpath=D:\Slides\hy\KB1902156\KB1902156-1_2019-04-06%2010_36_33.kfb&Zoom=20&FileNum=15&level=15&PositionX={0}&PositionY={1}&TileSize=256&gamma=1.2&contrast=1&light=0&rgbR=0&rgbG=0&rgbB=0".format(
+            X, Y)
+
+        response = requests.get(url)
+        data = response.content
+        # print(data)
+        name = str(X)+"_" + str(Y)
+        print(X)
+        file = open('./pic/{}.jpg'.format(name), 'wb')
+        file.write(data)
+        file.close()
